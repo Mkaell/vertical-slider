58930cbad1940b19283d9ff53a665372857bd938
Mkaell <mrohacevich@yandex.ru>
2021-12-25 14:57:19 +0300

    Readme.md added

---
+++ README.md
@@ -0,0 +1,16 @@
+# [Timer](https://mkaell.github.io/vertical-slider/)
+
+### _Application functionality:_ 
+This simple but beautiful slider switches slides by buttons on the page, and it is also possible to switch on the keyboard using: 'ArrowUp' and 'ArrowDown.
+
+### _Technologies used:_ 
+- The application is written using **JS**.
+- Icons added from **Font Awesome**.
+
+
+<div align="center">
+  <img src="./img/first.png" align="center" width='49%'>
+  <img src="./img/second.png"align="center" width='49%'>
+  <img src="./img/third.png"align="center" width='49%'>
+  <img src="./img/fourth.png"align="center" width='49%'>
+</div>
---
+++ js/script.js
@@ -0,0 +1,47 @@
+const upBtn = document.querySelector('.up-button'),
+      downBtn = document.querySelector('.down-button'),
+      sideBar = document.querySelector('.sidebar'),
+      container = document.querySelector('.container'),
+      mainSlide = document.querySelector('.main-slide'),
+      slidesCount = mainSlide.querySelectorAll('div').length;
+
+
+let activeSlideIndex = 0;  
+sideBar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+upBtn.addEventListener('click', () => {
+    changeSlide('up');
+});
+
+downBtn.addEventListener('click', () => {
+    changeSlide('down');
+});
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowUp') {
+        changeSlide('up');
+    } else if(event.key === 'ArrowDown') {
+        changeSlide('down');
+    }
+});
+
+function changeSlide(direction) {
+    if (direction === 'up') {
+        activeSlideIndex++;
+        if (activeSlideIndex === slidesCount) {
+            activeSlideIndex = 0;
+        }
+    } else if (direction === 'down'){
+        activeSlideIndex--;
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesCount - 1;
+        }
+    }
+
+    // Calculating the viewport height
+    const viewport = container.clientHeight;
+
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * viewport}px)`;
+    sideBar.style.transform = `translateY(${activeSlideIndex * viewport}px)`; 
+}
+
